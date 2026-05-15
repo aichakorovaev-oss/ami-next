69c3a47d884468ccf604ed057ee6c480b7764fd8
aichakorovaev-oss <aichakorovaev@gmail.com>
2026-05-15 02:38:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingIncludes: {
-    "/api/recommend": ["./data/**"],
-    "/api/health":    ["./data/**"],
-  },
-};
-module.exports = nextConfig;
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
-};
+const nextConfig = {};
 module.exports = nextConfig;
